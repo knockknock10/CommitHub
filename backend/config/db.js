001bee659f44038473e95d2c62894cd29563fb71
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-05-22 14:31:54 +0530

    db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error("MongoDB Error:", error.message);
+        console.error(`MongoDB Error: ${error.message}`);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
