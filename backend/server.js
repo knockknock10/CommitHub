f1df9e179728b0e12e4a7584b75e83bb55bc298c
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-06-26 22:10:25 +0530

    upfeat iniit backend server

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import cors from "cors"
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js";
-import repositoryRoutes
-from "./routes/repositoryRoutes.js";
+import repositoryRoutes from "./routes/repositoryRoutes.js";
+import issueRoutes from "./routes/issueRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+
 dotenv.config();
 
 const app = express();
@@ -18,35 +22,29 @@ app.use(cors());
 /* routes */
 
 app.use("/api/auth", authRoutes);
-app.use(
-    "/api/repositories",
-    repositoryRoutes
-);
+
+app.use("/api/repositories",repositoryRoutes);
+app.use("/api/issues",issueRoutes);
+
+app.use("/api/comments",commentRoutes);
+app.use("/api/users",userRoutes);
 
 app.get("/", (req, res) => {
-
     res.send("CommitHub API running");
 });
 
-/* server */
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-
     try{
-
         await connectDB();
-
         app.listen(PORT, () => {
-
             console.log(
                 `Server running on port ${PORT}`
             );
         });
-
     }catch(error){
-
         console.error(error);
     }
 };
