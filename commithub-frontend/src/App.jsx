b1f1d94723633842b0379be3bd1c9773144f8df8
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-06-28 22:09:56 +0530

    upfeat init routes

--- commithub-frontend/src/App.jsx
+++ commithub-frontend/src/App.jsx
@@ -5,28 +5,21 @@ import {
 } from "react-router-dom";
 
 import Landing from "./pages/Landing";
-
 import Login from "./components/auth/Login";
-
 import SignUp from "./components/auth/SignUp";
-
 import Dashboard from "./pages/Dashboard";
-
 import Repositories from "./pages/Repositories";
-
 import RepositoryDetails from "./pages/RepositoryDetails";
-
 import Issues from "./pages/Issues";
-
 import PullRequests from "./pages/PullRequests";
-
 import Activity from "./pages/Activity";
-
 import Settings from "./pages/Settings";
-
 import ProtectedRoute from "./routes/ProtectedRoute";
-
 import CreateRepoModal from "./components/repo/CreateRepoModal";
+import RepositoryPage from "./pages/RepositoryPage";
+import IssuePage from "./pages/IssuePage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
 
     return (
@@ -113,10 +106,34 @@ const App = () => {
                     }
                 />
                 <Route
-                    path ="/new"
+                    path="/new"
                     element={
                         <ProtectedRoute>
                             <CreateRepoModal />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/repo/:id"
+                    element={
+                        <ProtectedRoute>
+                            <RepositoryPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/issues/:id"
+                    element={
+                        <ProtectedRoute>
+                            <IssuePage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/profile/:id"
+                    element={
+                        <ProtectedRoute>
+                            <ProfilePage />
                         </ProtectedRoute>
                     }
                 />
