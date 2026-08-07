faf66fe2c0da8538956dbe641c4cd9f8cecc2313
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-08-07 18:38:48 +0530

    upfeat searchAPi front

---
+++ backend/routes/searchRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import protect from "../middleware/authmiddleware.js";
+import { globalSearch } from "../controllers/searchController.js";
+
+const router = express.Router();
+
+router.get(
+    "/",
+    protect,
+    globalSearch
+);
+
+export default router;
