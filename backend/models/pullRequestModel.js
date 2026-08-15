cdb8a6857e9fa380bd76843c78b608550ce016b0
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-08-15 12:41:56 +0530

    feat: add pull request model

---
+++ backend/models/pullRequestModel.js
@@ -0,0 +1,122 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const ReviewSchema = new Schema(
+    {
+        reviewer: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        state: {
+            type: String,
+            enum: ["approved", "changes_requested", "commented"],
+            required: true
+        },
+        comment: {
+            type: String,
+            default: ""
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const PullRequestCommentSchema = new Schema(
+    {
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const PullRequestSchema = new Schema(
+    {
+        number: {
+            type: Number,
+            required: true
+        },
+        repository: {
+            type: Schema.Types.ObjectId,
+            ref: "Repository",
+            required: true
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        sourceBranch: {
+            type: String,
+            required: true
+        },
+        targetBranch: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        status: {
+            type: String,
+            enum: ["open", "closed", "merged"],
+            default: "open"
+        },
+        reviews: {
+            type: [ReviewSchema],
+            default: []
+        },
+        comments: {
+            type: [PullRequestCommentSchema],
+            default: []
+        },
+        mergedAt: {
+            type: Date,
+            default: null
+        },
+        mergedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            default: null
+        },
+        mergeSourceCommitId: {
+            type: String,
+            default: null
+        },
+        mergeCommitId: {
+            type: String,
+            default: null
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+PullRequestSchema.index(
+    { repository: 1, number: 1 },
+    { unique: true }
+);
+
+const PullRequest = mongoose.model(
+    "PullRequest",
+    PullRequestSchema
+);
+
+export default PullRequest;
